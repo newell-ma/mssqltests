73030ab8296b9f688df9b35a651ed1a3bfdb80dd
newell-ma <58005547+newell-ma@users.noreply.github.com>
2025-03-31 22:39:55 +0100

    Add SQL Server test container support
    Updated `TestProject1Tests.csproj` to include `TestContainers` and `Testcontainers.MsSql` for integration testing. Introduced `_dbContainer` in `UnitTest1.cs` to manage the SQL Server container lifecycle with setup and teardown methods.

--- tests/TestProject1Tests/UnitTest1.cs
+++ tests/TestProject1Tests/UnitTest1.cs
@@ -1,7 +1,34 @@
+using Testcontainers.MsSql;
+
 namespace TestProject1Tests;
 
 public class Tests
 {
+    private MsSqlContainer _dbContainer;
+
+    [OneTimeSetUp]
+    public async Task BeforeAnyTests()
+    {
+        _dbContainer = new MsSqlBuilder()
+          .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+          .WithAcceptLicenseAgreement(true)
+          .Build();
+        await _dbContainer.StartAsync();
+
+        Console.WriteLine($"SQL Server is running at: {_dbContainer.GetConnectionString()}");
+    }
+
+    [OneTimeTearDown]
+    public async Task AfterAllTests()
+    {
+        if (_dbContainer != null)
+        {
+            await _dbContainer.StopAsync();
+            await _dbContainer.DisposeAsync();
+
+        }
+    }
+
     [SetUp]
     public void Setup()
     {
